2832b014b472cf2f910e4061e14eb168d04e4c00
Rooster <othmanmuhidean@gmail.com>
2019-12-15 14:24:00 -0800

    Creating initial variables

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    console.log(ready)
+
+
+    var json = {
+        title: "Code Quiz",
+        showProgressBar: "bottom",
+        showTimePanel: "top",
+        maxTimeToFinishPage: 15
+    }
+})
