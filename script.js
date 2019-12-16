1cc1038010e1a843aacebe9b724065c1a5eb386b
Rooster <othmanmuhidean@gmail.com>
2019-12-15 16:00:38 -0800

    creating functions

--- script.js
+++ script.js
@@ -1,11 +1,66 @@
 $(document).ready(function() {
-    console.log(ready)
+    console.log("ready");
+});
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
 
+function buildQuiz() {
+    var output = [];
+    questions.each((currentQuestion, questionNumber) => {
+        var answers = [];
+        for (letter in currentQuestion.answers) {
+            answers.push(
+                `<label>
+                      <input type="radio" name="question${questionNumber}" value="${letter}">
+                      ${letter} :
+                      ${currentQuestion.answers[letter]}
+                    </label>`
+            );
+        }
+        output.push(
+            `<div class="question"> ${currentQuestion.question} </div>
+                <div class="answers"> ${answers.join('')} </div>`
+        );
+    });
+    quizContainer.innerHTML = output.join('');
+};
 
-    var json = {
-        title: "Code Quiz",
-        showProgressBar: "bottom",
-        showTimePanel: "top",
-        maxTimeToFinishPage: 15
-    }
-})
+function showResults() {
+
+};
+buildQuiz();
+submitButton.on(showResults());
+var questions = [{
+        question: "",
+        answers: {},
+        correctAnswer: ""
+    },
+    {
+        question: "",
+        answers: {},
+        correctAnswer: ""
+    },
+    {
+        question: "",
+        answers: {},
+        correctAnswer: ""
+    },
+    {
+        question: "",
+        answers: {},
+        correctAnswer: ""
+    },
+    {
+        question: "",
+        answers: {},
+        correctAnswer: ""
+    },
+    {
+        question: "",
+        answers: {},
+        correctAnswer: ""
+    },
+]
+
+console.log(this);
